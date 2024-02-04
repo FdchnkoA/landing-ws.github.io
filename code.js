bde67c7ce768cc6bc3d62d7025554537856a006b
Andrew <104445536+FdchnkoA@users.noreply.github.com>
2024-02-04 17:22:23 +0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+function viewDiv(){
+    document.getElementById("container_signup").style.display = "block";
+    document.getElementById("container_signin").style.display = "none";
+};
+function viewDiv(){
+    document.getElementById("container_signin").style.display = "none";
+    document.getElementById("container_signup").style.display = "block";
+};
