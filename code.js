b5bb8a722080af2e35a48993940a32f96568b109
Andrew <104445536+FdchnkoA@users.noreply.github.com>
2024-02-04 19:32:12 +0500

    Delete code.js

--- code.js
+++
@@ -1,8 +0,0 @@
-function viewDiv(){
-    document.getElementById("container_signup").style.display = "block";
-    document.getElementById("container_signin").style.display = "none";
-};
-function viewDiv(){
-    document.getElementById("container_signin").style.display = "none";
-    document.getElementById("container_signup").style.display = "block";
-};
